db0dd6db0a9de13cb860cf11eb41db0e4189bc26
Luska18 <149702044+Luska18@users.noreply.github.com>
2024-06-20 17:33:17 -0300

    Add files via upload

---
+++ CoinFlip/CoinFlip/ViewModels/CoinViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoinFlip.Models;
+using System.Windows.Input;
+
+
+namespace CoinFlip.ViewModels
+{
+    public partial class CoinViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _lado;
+
+        [ObservableProperty]
+        public String _ladoEscolhido;
+
+        [ObservableProperty]
+        public String _imagem;
+
+        [ObservableProperty]
+        public String _resultado;
+
+        public ICommand FlipCommand { get; private set; }
+        
+        public CoinViewModel()
+        {
+            FlipCommand = new Command(Flip);
+        }
+
+        public void Flip()
+        {
+            Coin coin = new Coin();
+            Resultado = coin.Jogar(LadoEscolhido);
+            //Pegar o resultado da Moeda e escrever na label
+            Imagem = $"{coin.Lado}.png";
+        }
+    }
+}
---
+++ CoinFlip/CoinFlip/Views/CoinView.xaml.cs
@@ -0,0 +1,24 @@
+namespace CoinFlip.Views;
+using CoinFlip.Models;
+using CoinFlip.ViewModels;
+
+public partial class CoinView : ContentPage
+{
+	public CoinView()
+	{
+		InitializeComponent();
+		
+		this.BindingContext = new CoinViewModel();
+	}
+
+    private void CoinFlipButton_Clicked(object sender, EventArgs e)
+    {
+		string ladoEscolhido = HeadTailsPicker.SelectedItem.ToString();
+		//Jogar a moeda
+		Coin coin = new Coin();
+		string resultado = coin.Jogar(ladoEscolhido);
+        //Pegar o resultado da Moeda e escrever na label
+        ResultLabel.Text = resultado;
+		CoinImage.Source = $"{coin.Lado}.png";
+    }
+}
